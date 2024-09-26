79ed9c1046f307730232aeaa831c31de6538aaaf
atharva26aug <atharvamaskar10@gmail.com>
2024-09-26 22:26:21 +0530

    typecastnig

---
+++ 08.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    // Typecasting
+    float f = 69.69;
+    int i = (int)f;
+    printf("%d\n", i);
+
+    // To character
+    char c = (char)i;
+    printf("%c\n", c);
+}
